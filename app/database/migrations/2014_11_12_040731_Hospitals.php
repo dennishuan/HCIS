0b16cdbddc818bd1d74beef1721ded663888cc4b
eya11 <eya11@sfu.ca>
2014-11-11 20:44:40 -0800

    Added hospital table to migrations

---
+++ app/database/migrations/2014_11_12_040731_Hospitals.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Hospitals extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function(Blueprint $table)
+        {
+            $table->increments('patient_id')->unique();
+            $table->string('phn', 10)->unique();
+            $table->string('hospital_id', 3); 
+            $table->string('hospital_name');
+            $table->string('hospital_abbrev');
+            $table->date('reg_date');
+            $table->time('reg_time');
+            $table->date('admit_date');
+            $table->time('admit_time');
+            $table->enum('priority', array('1','2','3','4','5'));
+            $table->string('chief_compl_code', 10);
+            $table->string('chief_compl');
+            $table->string('stated_compl');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hospitals');
+    }
+
+}
