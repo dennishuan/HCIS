9f01cbab11c605167a1fbb29bf63ca748e251b92
aliaw <aliaw@sfu.ca>
2014-11-16 06:59:36 +0000

    integrate the view from Dennis

--- app/views/facility/edit.blade.php
+++ app/views/facility/edit.blade.php
@@ -6,118 +6,40 @@
     {{ Form::model($facility, ['method'=>'PUT', 'route'=>['facility.update', $facility->id]]) }}
 
         <div>
-            <dt>{{ Form::label('facility_name', 'facility_name: ') }}</dt>
-            <dd><div>{{ Form::text('facility_name' ) }}
-                {{ $errors->first('facility_name') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('name', 'Full Name: ') }}</dt>
-            <dd><div>{{ Form::input('text', 'name') }}
+            <dt>{{ Form::label('name', 'Facility Name: ') }}</dt>
+            <dd><div>{{ Form::text('name' ) }}
                 {{ $errors->first('name') }}</div></dd>
-       </div>
-
-        <div>
-            <dt>{{ Form::label('preferred_name', 'Preferred Name: ') }}</dt>
-            <dd><div>{{ Form::input('text', 'preferred_name') }}
-                {{ $errors->first('preferred_name') }}</div></dd>
-       </div>
-
-        <div>
-            <dt>{{ Form::label('sex', 'Sex: ') }}</dt>
-            <dd><div>{{ Form::text('sex' ) }}
-                {{ $errors->first('sex') }}</div></dd>
         </div>
 
         <div>
-            <dt>{{ Form::label('date_of_birth', 'Date of Birth: ') }}</dt>
-            <dd><div>{{ Form::text('date_of_birth' ) }}
-                {{ $errors->first('date_of_birth') }}</div></dd>
+            <dt>{{ Form::label('abbrev', 'Facility Abbreviation: ') }}</dt>
+            <dd><div>{{ Form::text('abbrev') }}
+                {{ $errors->first('abrev') }}</div></dd>
         </div>
+
+        <div>
+            <dt>{{ Form::label('type', 'Facility Type: ') }}</dt>
+            <dd><div>{{ Form::input('enum', 'type') }}
+                {{ $errors->first('type') }}</div></dd>
+        </div>
+
+        <div>
+            <dt>{{ Form::label('phone', 'Phone Number: ') }}</dt>
+            <dd><div>{{ Form::text('phone') }}
+                {{ $errors->first('phone') }}</div></dd>
+        </div>
+
 
         <div>
             <dt>{{ Form::label('address', 'Address: ') }}</dt>
-            <dd><div>{{ Form::text('address' ) }}
+            <dd><div>{{ Form::text('address') }}
                 {{ $errors->first('address') }}</div></dd>
         </div>
 
         <div>
             <dt>{{ Form::label('postal_code', 'Postal Code: ') }}</dt>
-            <dd><div>{{ Form::text('postal_code' ) }}
+            <dd><div>{{ Form::text('postal_code') }}
                 {{ $errors->first('postal_code') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('home_phone', 'Phone Number: ') }}</dt>
-            <dd><div>{{ Form::text('home_phone' ) }}
-                {{ $errors->first('home_phone') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('work_phone', 'Work phone Number: ') }}</dt>
-            <dd><div>{{ Form::text('work_phone' ) }}
-                {{ $errors->first('work_phone') }}</div></dd>
-        </div>
-
-
-        <div>
-            <dt>{{ Form::label('mobile_phone', 'Mobile Phone Number: ') }}</dt>
-            <dd><div>{{ Form::text('mobile_phone' ) }}
-                {{ $errors->first('mobile_phone') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('emergency_name', 'Emergency Contact Name: ') }}</dt>
-            <dd><div>{{ Form::text('emergency_name' ) }}
-                {{ $errors->first('emergency_name') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('emergency_phone', 'Emergency Contact Phone Number: ') }}</dt>
-            <dd><div>{{ Form::text('emergency_phone' ) }}
-                {{ $errors->first('emergency_phone') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('emergency_relationship', 'Emergency Contact Relationship: ') }}</dt>
-            <dd><div>{{ Form::text('emergency_relationship' ) }}
-                {{ $errors->first('emergency_relationship') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('allergies', 'Allergies: ') }}</dt>
-            <dd><div>{{ Form::text('allergies' ) }}
-                {{ $errors->first('allergies') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('permanent_resident', 'Permanent Resident: ') }}</dt>
-            <dd><div>{{ Form::text('permanent_resident' ) }}
-                {{ $errors->first('permanent_resident') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('preferred_language', 'Preferred Language: ') }}</dt>
-            <dd><div>{{ Form::text('preferred_language' ) }}
-                {{ $errors->first('preferred_language') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('other_language', 'Other Language: ') }}</dt>
-            <dd><div>{{ Form::text('other_language' ) }}
-                {{ $errors->first('other_language') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('ethnic_background', 'Ethnic Background: ') }}</dt>
-            <dd><div>{{ Form::text('ethnic_background' ) }}
-                {{ $errors->first('ethnic_background') }}</div></dd>
-        </div>
-
-        <div>
-            <dt>{{ Form::label('family_doctor', 'Family Doctor: ') }}</dt>
-            <dd><div>{{ Form::text('family_doctor' ) }}
-                {{ $errors->first('family_doctor') }}</div></dd>
         </div>
 
         <div>
--- app/views/facility/show.blade.php
+++ app/views/facility/show.blade.php
@@ -5,62 +5,23 @@
     <h1>Facility:</h1>
 
     <dl>
-        <dt>facility_name:</dt>
-        <dd><div>{{ $facility->facility_name }}</div></dd>
+        <dt>Abbrev.: </dt>
+        <dd><div>{{ $facility->abbrev }}</div></dd>
 
-        <dt>Full Name:</dt>
+        <dt>Name: </dt>
         <dd><div>{{ $facility->name }}</div></dd>
 
-        <dt>Preferred Name:</dt>
-        <dd><div>{{ $facility->preferred_name }}</div></dd>
+        <dt>Type: </dt>
+        <dd><div>{{ $facility->type }}</div></dd>
 
-        <dt>Sex:</dt>
-        <dd><div>{{ $facility->sex }}</div></dd>
+        <dt>Phone: </dt>
+        <dd><div>{{ $facility->phone }}</div></dd>
 
-        <dt>Date of Birth:</dt>
-        <dd><div>{{ $facility->date_of_birth }}</div></dd>
-
-        <dt>Address:</dt>
+        <dt>Address: </dt>
         <dd><div>{{ $facility->address }}</div></dd>
 
-        <dt>Postal Code:</dt>
+        <dt>Postal Code: </dt>
         <dd><div>{{ $facility->postal_code }}</div></dd>
-
-        <dt>Home Phone:</dt>
-        <dd><div>{{ $facility->home_phone }}</div></dd>
-
-        <dt>Work Phone:</dt>
-        <dd><div>{{ $facility->work_phone }}</div></dd>
-
-        <dt>Mobile Phone:</dt>
-        <dd><div>{{ $facility->mobile_phone }}</div></dd>
-
-        <dt>Emergency Contact Name:</dt>
-        <dd><div>{{ $facility->emergency_name }}</div></dd>
-
-        <dt>Emergency Contact Phone Number:</dt>
-        <dd><div>{{ $facility->emergency_phone }}</div></dd>
-
-        <dt>Emergency Contact Relationship:</dt>
-        <dd><div>{{ $facility->emergency_relationship }}</div></dd>
-
-        <dt>Allergies:</dt>
-        <dd><div>{{ $facility->allergies }}</div></dd>
-
-        <dt>Permanent Resident:</dt>
-        <dd><div>{{ $facility->permanent_resident }}</div></dd>
-
-        <dt>Preferred Language:</dt>
-        <dd><div>{{ $facility->preferred_language }}</div></dd>
-
-        <dt>Other Language:</dt>
-        <dd><div>{{ $facility->other_language }}</div></dd>
-
-        <dt>Ethnic Background:</dt>
-        <dd><div>{{ $facility->ethnic_background }}</div></dd>
-
-        <dt>Family Doctor:</dt>
-        <dd><div>{{ $facility->family_doctor }}</div></dd>
     </dl>
 
     <nav>
