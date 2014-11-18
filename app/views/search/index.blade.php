c272ba63d21f02c0fdea21fe751da85c0ec73e54
Dennis <djh10@sfu.ca>
2014-11-17 19:38:55 -0800

    Added records to search view

--- app/views/search/index.blade.php
+++ app/views/search/index.blade.php
@@ -24,22 +24,47 @@
 				<!--Left side of form-->
 				<div class="col col-md-6 col-lg-6">
 					<div class="input-group">
-						<span class="input-group-addon">Abbreviation:</span>
-						{{ Form::text('abbrev', null, ['class' => 'form-control'] ) }}
+						<span class="input-group-addon">Patient ID:</span>
+						{{ Form::text('patient_id', null, ['class' => 'form-control'] ) }}
 					</div>
 
 					<div class="input-group buffer">
-						<span class="input-group-addon">Name:</span>
-						{{ Form::text('name' , null, ['class' => 'form-control'] ) }}
+						<span class="input-group-addon">User ID:</span>
+						{{ Form::text('user_id' , null, ['class' => 'form-control'] ) }}
 					</div>
 
 					<div class="input-group buffer">
-						<span class="input-group-addon">Phone:</span>
-						{{ Form::text('phone' , null, ['class' => 'form-control'] ) }}
+						<span class="input-group-addon">Registration:</span>
+						{{ Form::text('reg_datetime' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Stated Complaint:</span>
+						{{ Form::text('stated_compl' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Chief Complaint Code:</span>
+						{{ Form::text('chief_compl_code' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Subjective:</span>
+						{{ Form::text('subjective' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Assessment:</span>
+						{{ Form::text('assessment' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Remarks:</span>
+						{{ Form::text('remarks' , null, ['class' => 'form-control'] ) }}
 					</div>
 
 					<div class="input-group buffer">
-						{{ Form::submit('Search for Facility', ['class' => 'btn btn-info'])}}
+						{{ Form::submit('Search for Record', ['class' => 'btn btn-info'])}}
 					</div>
 				</div>
 				<!-- End left side of form -->
@@ -48,29 +73,50 @@
 				<div class="col col-md-6 col-lg-6">
 				
 					<div class="input-group">
-						<span class="input-group-addon">Fax:</span>
+						<span class="input-group-addon">Facility ID:</span>
 						{{ Form::text('fax' , null, ['class' => 'form-control'] ) }}
 					</div>
 					
 					<div class="input-group buffer">
-						<span class="input-group-addon">Address:</span>
-						{{ Form::text('address' , null, ['class' => 'form-control'] ) }}
+						<span class="input-group-addon">Priority:</span>
+						<select class="form-control" name="priority">
+							<option value="">All</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+						</select>
 					</div>
 					
 					<div class="input-group buffer">
-						<span class="input-group-addon">Postal Code:</span>
-						{{ Form::text('postal_code' , null, ['class' => 'form-control'] ) }}
+						<span class="input-group-addon">Time of Admittance:</span>
+						{{ Form::text('admit_datetime' , null, ['class' => 'form-control'] ) }}
 					</div>
+					
 					<div class="input-group buffer">
-						<span class="input-group-addon">Type:</span>
-						<select class="form-control" name="type">
-							<option value="">All</option>
-								@if (Auth::user() != null && Auth::user()->isAdmin())
-									<option value="admin">Admin</option>
-								@endif
-							<option value="hospital">Hopstial</option>
-							<option value="clinic">Clinic</option>
-						</select>
+						<span class="input-group-addon">Chief Complaint:</span>
+						{{ Form::text('chief_compl' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Arrival Mode:</span>
+						{{ Form::text('arrival_mode' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Objective:</span>
+						{{ Form::text('objective' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Prescription:</span>
+						{{ Form::text('prescription' , null, ['class' => 'form-control'] ) }}
+					</div>
+					
+					<div class="input-group buffer">
+						<span class="input-group-addon">Plan:</span>
+						{{ Form::text('plan' , null, ['class' => 'form-control'] ) }}
 					</div>
 				</div>
 				<!-- End right side of form -->
@@ -152,7 +198,7 @@
 				<!-- Right side of form -->
 				<div class="col col-md-6 col-lg-6">
 				
-					<div class="input-group buffer">
+					<div class="input-group">
 						<span class="input-group-addon">Preferred Name:</span>
 						{{ Form::text('preffered_name' , null, ['class' => 'form-control'] ) }}
 					</div>
@@ -171,22 +217,22 @@
 						{{ Form::text('address' , null, ['class' => 'form-control'] ) }}
 					</div>
 				
-					<div class="input-group">
+					<div class="input-group buffer">
 						<span class="input-group-addon">Home Phone:</span>
 						{{ Form::text('home_phone' , null, ['class' => 'form-control'] ) }}
 					</div>
 					
-					<div class="input-group">
+					<div class="input-group buffer">
 						<span class="input-group-addon">Mobile Phone:</span>
 						{{ Form::text('mobile_phone' , null, ['class' => 'form-control'] ) }}
 					</div>
 					
-					<div class="input-group">
+					<div class="input-group buffer">
 						<span class="input-group-addon">Emergency Name:</span>
 						{{ Form::text('emergency_name' , null, ['class' => 'form-control'] ) }}
 					</div>
 					
-					<div class="input-group">
+					<div class="input-group buffer">
 						<span class="input-group-addon">Emergency Relationship:</span>
 						{{ Form::text('emergency_relationship' , null, ['class' => 'form-control'] ) }}
 					</div>
@@ -195,6 +241,7 @@
 					<div class="input-group buffer">
 						<span class="input-group-addon">Permanent Resident:</span>
 						<select class="form-control" name="permanent_resident">
+							<option value="">All</option>
 							<option value="1">Yes</option>
 							<option value="0">No</option>
 						</select>
@@ -205,7 +252,7 @@
 						{{ Form::text('preferred_lnguage' , null, ['class' => 'form-control'] ) }}
 					</div>
 					
-					<div class="input-group">
+					<div class="input-group buffer">
 						<span class="input-group-addon">Ethnic Background:</span>
 						{{ Form::text('ethnic_background' , null, ['class' => 'form-control'] ) }}
 					</div>
