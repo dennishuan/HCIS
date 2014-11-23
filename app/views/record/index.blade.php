c37ac01f91200c681af8673229f14101efadc46c
aliaw <aliaw@sfu.ca>
2014-11-23 18:32:43 +0000

    add deletion confirm dailog for index pages

--- app/views/record/index.blade.php
+++ app/views/record/index.blade.php
@@ -4,6 +4,25 @@
 
 @section('content')
 
+<!-- Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="deleteModalLabel">Confirm Deletion</h4>
+      </div>
+      <div class="modal-body">
+          <p>Do you want to delete the selected entry?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Back</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal" id="delete">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <!--Button-->
 <div id="toolbar" class="btn-group btn-default">
   <button class="btn btn-default" id="create">
@@ -15,11 +34,12 @@
   <button class="btn btn-default" id="edit">
     <i class="glyphicon glyphicon-edit"></i> <span>Edit</span>
   </button>
-  <button class="btn btn-default" id="delete">
+  <button class="btn btn-default" data-toggle="modal" data-target="#deleteModal">
     <i class="glyphicon glyphicon-trash"></i> <span>Delete</span>
   </button>
 
 </div>
+
 
 
 <!--Table-->
