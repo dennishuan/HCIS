bd3cd1d9d6c21d2dff06819cd703a5285a81c67b
aliaw <aliaw@sfu.ca>
2014-11-24 03:04:04 +0000

    Fix typo in create/edit facility

--- app/views/facility/edit.blade.php
+++ app/views/facility/edit.blade.php
@@ -22,9 +22,9 @@
 
       <div class="input-group buffer">
         <span class="input-group-addon">Address:</span>
-        {{ Form::text('phone', $facility->phone, ['class' => 'form-control']) }}
+        {{ Form::text('Address', $facility->address, ['class' => 'form-control']) }}
       </div>
-      {{ $errors->first('phone')}}
+      {{ $errors->first('Address')}}
 
       <div class="input-group buffer">
         {{ Form::submit('Make Changes', ['class' => 'btn btn-info'])}}
