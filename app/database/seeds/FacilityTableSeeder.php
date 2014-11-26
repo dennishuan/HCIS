9508ba50b9883bd0b8a2ac85fd9afb26fe490cb6
aliaw <aliaw@sfu.ca>
2014-11-26 22:58:56 +0000

    Add IMPORT user and facility

--- app/database/seeds/FacilityTableSeeder.php
+++ app/database/seeds/FacilityTableSeeder.php
@@ -4,24 +4,36 @@ use Faker\Factory as Faker;
 
 class FacilityTableSeeder extends Seeder {
 
-    public function run()
-    {
-        $faker = Faker::create('en_CA');
+	public function run()
+	{
+		$faker = Faker::create('en_CA');
 
-        $type = ['Hospital','Clinic'];
+		$type = ['Hospital','Clinic'];
 
-        foreach(range(1, 8) as $index)
-        {
-            Facility::create([
-                'abbrev' => $faker->lexify('??????'),
-                'name' => $faker->company,
-                'type' => $faker->randomElement($type),
-                'phone' => $faker->numerify('##########x####'),
-                'fax' => $faker->numerify('##########x####'),
-                'address' => $faker->address,
-                'postal_code' => strtoupper ($faker->bothify('#?##?#'))
-            ]);
-        }
-    }
+		foreach(range(1, 8) as $index)
+		{
+			Facility::create([
+				'abbrev' => $faker->lexify('??????'),
+				'name' => $faker->company,
+				'type' => $faker->randomElement($type),
+				'phone' => $faker->numerify('##########x####'),
+				'fax' => $faker->numerify('##########x####'),
+				'address' => $faker->address,
+				'postal_code' => strtoupper ($faker->bothify('#?##?#'))
+			]);
+		}
+
+		Facility::create([
+			'abbrev' => 'IMPORT',
+			'name' => 'IMPORT',
+			'type' => 'Hospital',
+			'phone' => '0000000000',
+			'fax' => '0000000000',
+			'address' => 'IMPORT',
+			'postal_code' => 'IMPORT',
+		]);
+
+		FAcility::where('name', 'IMPORT')->update(['id' => 0]);
+	}
 
 }
