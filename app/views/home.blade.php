796cc04e8ea09d62da00970d8d8d81bc3724e451
jhf1 <jhf1@sfu.ca>
2014-11-29 14:58:18 -0800

    fix homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -13,16 +13,16 @@
 
 @section('content')
 
-Welcome to the home page. To begin, please click on the tabs at the top.
+<p>Welcome to the home page. To begin, please click on the tabs at the top.</p>
 
-Record: To create, edit, and delete medical records of patients related to your facility. You can also view the full patient's information.
+<p>Record: To create, edit, and delete medical records of patients related to your facility. You can also view the full patient's information.</p>
 
-Patient: To view the list of existing patients in your facility. Only the Admins have upload and export control functionalities.
+<p>Patient: To view the list of existing patients in your facility. Only the Admins have upload and export control functionalities.</p>
 
-User: To view the list of users, with the options to see their profiles, in addition to creating new, editing, and deleting new users (admins only).
+<p>User: To view the list of users, with the options to see their profiles, in addition to creating new, editing, and deleting new users (admins only).</p>
 
-Facility: The types of medical facility: Hospitals and medical clinics. This shows the list of facilities stored in the database, with the option to view, create, edit, and delete each facility.
+<p>Facility: The types of medical facility: Hospitals and medical clinics. This shows the list of facilities stored in the database, with the option to view, create, edit, and delete each facility.</p>
 
-Search: Allows users to comprehensively search for a specific record, patient, and/or medical doctor based on the field filled out in the records, patients, users, or facility options.
+<p>Search: Allows users to comprehensively search for a specific record, patient, and/or medical doctor based on the field filled out in the records, patients, users, or facility options.</p>
 
 @stop
